85a9cc459fcc8d4cc17734c33c872eb9e7d634c2
akash rawat <akash.rawat@enine.school>
2024-02-21 12:08:29 +0530

    Completed

--- energy_scalar/answer.py
+++ energy_scalar/answer.py
@@ -25,11 +25,12 @@ def find_monthly_hourly_data(month_data):
     """
     return the energy scalar monthly
     """
+    peak_avg, off_peak_avg = 0, 0
     monthly_hourly_data = {}
     for index in range(0, len(month_data), 24):
         day_data = month_data[index : index + 24]
-        peak_avg = find_peak_avg(day_data)
-        off_peak_avg = find_off_peak_avg(day_data)
+        peak_avg += find_peak_avg(day_data)
+        off_peak_avg += find_off_peak_avg(day_data)
 
         for index, hour in enumerate(day_data):
             current_date = hour["date"]
@@ -37,13 +38,17 @@ def find_monthly_hourly_data(month_data):
             if dt.hour not in monthly_hourly_data:
                 monthly_hourly_data[dt.hour] = 0
 
-            if 5 < index < 22:
-                monthly_hourly_data[dt.hour] += hour["price"] / float(peak_avg)
-            else:
-                monthly_hourly_data[dt.hour] += hour["price"] / float(off_peak_avg)
+            monthly_hourly_data[dt.hour] += hour["price"]
 
-    for data in monthly_hourly_data:
+    peak_avg /= len(month_data) // 24
+    off_peak_avg /= len(month_data) // 24
+    for index, data in enumerate(monthly_hourly_data):
         monthly_hourly_data[data] /= len(month_data) // 24
+
+        if 5 < index < 22:
+            monthly_hourly_data[data] /= peak_avg
+        else:
+            monthly_hourly_data[data] /= off_peak_avg
 
     return monthly_hourly_data
 
